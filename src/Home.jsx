ce8d28847d1ac79be9703e3474ac5e27276d2eee
SnehalKherkar <snehalkherkar06@gmail.com>
2024-12-14 16:09:10 +0530

    firstChange

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Snehal</h1>
+      <h1>Snehal</h1>
+      <h1>Jakir</h1>
+    </div>
+  )
+}
+
+export default Home
